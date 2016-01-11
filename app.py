0004c0a607593ab59f9bc71e7299f2943d13b9ca
Justin Pacquing <pacqqu@gmail.com>
2016-01-11 12:16:56 -0500

    lololol

--- app.py
+++ app.py
@@ -2,5 +2,18 @@ import jinja2
 from flask import Flask
 from flask import redirect, render_template, request, session
 
-from server.database_manager import DatabaseManager
-from server.util import Util
+from db import DatabaseManager
+from util import Util
+
+app = Flask(__name__)
+dbm = DatabaseManager.create()
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+  return render_template('index.html')
+
+if __name__ == '__main__':
+  app.debug = True
+  app.run(host='0.0.0.0',port=8000)
